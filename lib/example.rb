622fa8b4e63b6f74f010b2f5f9445a7d6dd49417
Chris ward <wardc2126@gmail.com>
2019-12-14 17:35:44 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,10 +1,10 @@
 def say_hello_world_ten_times
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 puts phrase
 puts phrase
 puts phrase
